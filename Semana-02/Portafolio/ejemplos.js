6ed9973ef4f4a73c29b21076d0254951a93c89f1
Sebastián Chavarry <69838707+sebastianperudev2001@users.noreply.github.com>
2022-01-18 15:55:00 -0500

    Primer commit

---
+++ Semana-02/Portafolio/ejemplos.js
@@ -0,0 +1,29 @@
+document.write("<h1> Hola </h1>")
+
+//Funcion
+
+function clickMeOnClick()
+{
+    console.log("You clicked me :O")
+}
+
+
+const numero = 1.4;
+const nombre = "Billy"; 
+
+//Es mejor usar let en lugar de var
+//Mientras puedas utiliza const, porque varias veces vas a reutilizar el mismo valor
+
+//puedes mutar el contenido del arreglo 
+
+const diasSemana = ["Lunes", "Martes", "Miercoles"];
+
+//UN objeto en JAvascript es como un diccionario 
+
+const person = 
+{
+    nombre: "Billy",
+    edad: 35
+}
+
+//La variable toma el tipo de dato del valor asignado
