6dc31916d82216e7d91d575eda15f2aa94d61831
Sebastián Chavarry <69838707+sebastianperudev2001@users.noreply.github.com>
2022-01-18 16:51:45 -0500

    falta corregir errores de OperacionOnclick

--- Semana-02/Calculadora/calculadora.js
+++ Semana-02/Calculadora/calculadora.js
@@ -1,8 +1,11 @@
+let numeroIngresadoStr = 0; //global
+
 function digitOnClick(numero) {
     const input = document.getElementById("txt_numeros");
-    if (input.value == "0") 
+    if (input.value == "0" || enOperacion) 
     {
         input.value = numero;     
+        enOperacion = false; 
     } else 
     {
         input.value += numero; 
@@ -15,5 +18,14 @@ function operacionOnClick(tipoOperacion)
     {
         const input = document.getElementById("txt_numeros");
         input.value = 0;     
-    } 
+    } else if (tipoOperacion == "+") 
+    {
+        numeroIngresadoStr = input.value; 
+        enOperacion = true; 
+
+    } else if (tipoOpereacion == "=") 
+    {
+        const resultado = parseInt(input.value) + parseInt(numeroIngresadoStr);
+        input.value = resultado + "";
+    }
 }
