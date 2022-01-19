05fdc1380331a31f51fd4de5b20a112d01da4768
Sebastián Chavarry <69838707+sebastianperudev2001@users.noreply.github.com>
2022-01-19 15:14:16 -0500

    Versión funcional del profe de calculadora.js. Funciona la suma y resultado y C

--- Semana-02/Calculadora/calculadora.js
+++ Semana-02/Calculadora/calculadora.js
@@ -1,31 +1,27 @@
-let numeroIngresadoStr = 0; //global
+let numeroIngresadoStr = 0 // global
+let enOperacion = false;
 
 function digitOnClick(numero) {
-    const input = document.getElementById("txt_numeros");
-    if (input.value == "0" || enOperacion) 
-    {
-        input.value = numero;     
-        enOperacion = false; 
-    } else 
-    {
-        input.value += numero; 
+    const input = document.getElementById("txt_numeros")
+    
+    if (input.value == "0" || enOperacion) {
+        input.value = numero
+        enOperacion = false
+    }else {
+        //input.value = input.value + numero
+        input.value += numero
     }
 }
 
-function operacionOnClick(tipoOperacion) 
-{
-    if (tipoOperacion == "C") 
-    {
-        const input = document.getElementById("txt_numeros");
-        input.value = 0;     
-    } else if (tipoOperacion == "+") 
-    {
-        numeroIngresadoStr = input.value; 
-        enOperacion = true; 
-
-    } else if (tipoOpereacion == "=") 
-    {
-        const resultado = parseInt(input.value) + parseInt(numeroIngresadoStr);
-        input.value = resultado + "";
+function operacionOnClick(tipoOperacion) {
+    const input = document.getElementById("txt_numeros")
+    if (tipoOperacion == "C") {
+        input.value = "0"
+    }else if (tipoOperacion == "+") {
+        numeroIngresadoStr = input.value
+        enOperacion = true;
+    }else if (tipoOperacion == "=") {
+        const resultado = parseInt(input.value) + parseInt(numeroIngresadoStr)
+        input.value = resultado.toString()
     }
 }
