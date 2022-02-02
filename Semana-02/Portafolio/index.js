d0f55e9dc9d2c1554f84df6a46287571251d1192
Sebastián Chavarry <69838707+sebastianperudev2001@users.noreply.github.com>
2022-02-02 15:16:56 -0500

    Ejercicio de AJAX

--- Semana-02/Portafolio/index.js
+++ Semana-02/Portafolio/index.js
@@ -29,6 +29,7 @@ const callbackSuccess = (response) =>
 
 const callbackReadResponse = (data) => 
 {
+    console.log("termino carga de imagenes")
     const arrIMagenes = data.images;
     let esPrimera = true; 
     for(let url of arrIMagenes) 
@@ -46,8 +47,7 @@ const callbackError = (error) =>
 }
 
 
-
-const main = () => 
+const cargarImagenes = () => 
 {
     //se abre un canal de comunicación con una URL y se pueden enviar datos.
     fetch("http://demo9667197.mockable.io/")
@@ -57,4 +57,55 @@ const main = () =>
         //2 posibles resultados: salga bien o error. Debe haber código para responder ambos casos.
 }
 
+const cargarProyectos = () => 
+{
+    fetch("http://demo9667197.mockable.io/proyectos")
+        .then( (response) => {
+            return response.json()
+        })
+        .then ( (data) => {
+            console.log("termino de cargar proyecto")
+            console.log(data)
+        })
+        .catch(callbackError)
+}
+
+
+//Cuando usamos await, la funcion padre debe tener la palabra clave ASYNC
+const cargarDatosAsyncAwait = async () => 
+{
+    //Es como si actuarán secuencialmente, pero el navegador lo toma como si fuera
+    
+    const response = await fetch("http://demo9667197.mockable.io/"); //HAGO LA LLAMADA
+    const data = await response.json(); //LO VUELVO OBJETO JS 
+
+    console.log("termino carga de imagenes")
+    const arrIMagenes = data.images;
+    let esPrimera = true; 
+    for(let url of arrIMagenes) 
+    {
+        const div = crearDivImagenCarrusel(url, esPrimera)
+        document.getElementById("carrusel").appendChild(div); 
+        esPrimera = false; 
+    }
+
+    const responseProyecto = await fetch("http://demo9667197.mockable.io/proyectos")
+    const dataProyectos = await responseProyecto.json()
+    console.log("termino de cargar proyecto")
+    console.log(data)
+
+
+
+}
+
+const main = () => 
+{
+    cargarDatosAsyncAwait();
+    //Si es una comunicación asíncrona, eso quiere decir que no es lineal
+    //Promesa --> Fetch. no devuelve exactamente el control de la ejecución al navegador, devuelve una conexión
+}
+
 window.addEventListener("load",main); 
+
+
+//Es posible poner un fetch dentro de otro fetch para ejecutar datos primeros que otros
